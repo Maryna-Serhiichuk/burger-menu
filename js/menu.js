34cc28ce0006e735f86093e15d9f4a3ef440e1fb
Maryna-Serhiichuk <marina.serhiichuk@gmail.com>
2021-09-24 12:28:29 +0300

    del

--- js/menu.js
+++ js/menu.js
@@ -3,7 +3,4 @@ $(document).ready(function(){
     $('.click, .burger__menu, .img__logo-max').toggleClass('active');
     $('.body').toggleClass('lock');
   })
-  $('.img__logo-max').click(function(){
-    $('.info, .project').toggleClass('show');
-  })
 })
